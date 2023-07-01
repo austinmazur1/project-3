185350e70f1ae6a223c5e8305cdbd6004040a35f
Asier González <asiergontz8@gmail.com>
2023-07-01 08:53:32 +0200

    product and buyer models

--- Server/server/models/User.model.js
+++ Server/server/models/User.model.js
@@ -22,7 +22,8 @@ const userSchema = new Schema(
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
-  }
+  },
+  
 );
 
 const User = model("User", userSchema);
